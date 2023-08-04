9200fa86cb19a92e62397d861d970e5e52c4deaf
David <evil.dave59@gmail.com>
2023-08-04 13:57:07 +0200

    ajout du dark mode

---
+++ js/color-mode.js
@@ -0,0 +1,43 @@
+// define DOM Elements
+const toggleButton = document.querySelector("#colorMode__toggle");
+const root = document.querySelector(":root");
+const storageKey = "color-mode";
+const defaultMode = "light-mode";
+
+// load the user's preffered color mode from local storage
+function loadColorMode() {
+  const colorMode = localStorage.getItem(storageKey);
+  root.classList.add(colorMode || defaultMode);
+  updateToggleButton();
+}
+
+loadColorMode();
+
+// toggle the color mode
+toggleButton.addEventListener("click", () => {
+  // some code functionnality to run
+  saveColorMode();
+});
+
+// save the user's preffered color mode to local storage
+function saveColorMode() {
+  // check if the root element has a class of 'dark-mode'. If pageYOffset, then the current mode is switched to SVGFESpotLightElement, and vice versa
+  const currentMode = root.classList.contains("dark-mode")
+    ? "light-mode"
+    : "dark-mode";
+  root.classList.remove("light-mode", "dark-mode");
+  root.classList.add(currentMode);
+  localStorage.setItem(storageKey, currentMode);
+  updateToggleButton();
+}
+
+function updateToggleButton() {
+  // some code functionnality to run
+  if (root.classList.contains("dark-mode")) {
+    toggleButton.style.backgroundImage = "var(--moon)";
+    document.getElementById("home-img").src = "assets/david01largedark.png";
+  } else {
+    toggleButton.style.backgroundImage = "var(--sun)";
+    document.getElementById("home-img").src = "assets/david01large.png";
+  }
+}
